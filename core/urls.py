6d997d9af0c18140834fe11052a9aacca6274a33
ankitkarki27 <ankitkarki8088@gmail.com>
2025-09-15 20:17:18 +0545

    zango-post

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from post.views import *
-from post.views import home_view
+from post.views import home_view,post_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view)
+    path('', home_view, name='home'),
+    path('post/create/', post_create_view, name='post-create'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from django.forms import ModelForm
+from django import forms
 
-# Create your views here.
 def home_view(request):
-    # print(request)
-    title = 'welcome to Zango'
-    return render(request, 'post/home.html', {'title': title})
+    # title = 'Zango here'
+    posts=Post.objects.all().order_by('-created_at')
+    return render(request, 'post/home.html', {'posts': posts})
+
+class PostCreateForm(ModelForm):
+    class Meta:
+        model=Post
+        fields='__all__'
+        labels ={
+            'image': 'Image URL',
+            'caption': 'Caption',
+            'description': 'Description',
+        }
+        widgets = {
+            'caption': forms.TextInput(attrs={'placeholder': 'Enter caption here','class':'font-normal text-sm'}),
+            'image': forms.TextInput(attrs={'placeholder': 'Enter image URL here','class':'font-normal text-sm'}),
+            'description': forms.Textarea(attrs={'placeholder': 'Enter description here','class':'font-normal text-sm'}),
+        }
+        
+def post_create_view(request):
+    form= PostCreateForm()
+    
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form= PostCreateForm()
+            return redirect('home')
+            
+    return render(request, 'post/post_create.html', {'form': form})
